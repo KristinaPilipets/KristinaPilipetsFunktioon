1b510fbe00d4f3721859dca2da3abe1e35082637
opilane <opilane@HP-CZC6297BB1>
2021-11-09 12:51:59 +0200

    ...

--- KristinaPilipetsFunktioon.py
+++ KristinaPilipetsFunktioon.py
@@ -2,6 +2,11 @@ from random import*
 from keyboard import*
 #пример с ботами
 def start():
+	"""Teeme valik kellega mängima
+	tagastame m muutja int formaadis
+	
+	:rtype: int
+	"""
 	print("kivi, käärid, paber")
 	m=3
 	while m not in [1,2]:
